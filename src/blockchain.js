b17fa7c2997de05e1890a3c8d8246f230796996d
jc-torresp <jc.torresprz@gmail.com>
2021-06-15 10:16:32 -0500

    getBlockByHash method using find function instead of filter.

--- src/blockchain.js
+++ src/blockchain.js
@@ -137,7 +137,7 @@ class Blockchain {
     getBlockByHash(hash) {
         let self = this;
         return new Promise((resolve, reject) => {
-           const block = self.chain.filter(p => p.hash === hash)[0];
+           const block = self.chain.find(p => p.hash === hash);
             if(block){
                 resolve(block);
             } else {
